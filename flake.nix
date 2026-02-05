335ad08643e02139f61d88d66c981ba41f5aa02d
CryptoGladi <dam.almaev@gmail.com>
2026-02-05 19:55:59 +0300

    Init

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+
+    flake-utils.url = "github:numtide/flake-utils";
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    naersk = {
+      url = "github:nix-community/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    flake-utils,
+    naersk,
+    nixpkgs,
+    rust-overlay,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        overlays = [(import rust-overlay)];
+        pkgs = (import nixpkgs) {
+          inherit system overlays;
+        };
+
+        naersk' = pkgs.callPackage naersk {};
+
+        rust = pkgs.rust-bin.stable.latest.default;
+      in {
+        # For `nix build` & `nix run`:
+        packages.default = naersk'.buildPackage {
+          src = ./.;
+        };
+
+        # For `nix develop`:
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = [rust];
+
+          shellHook = ''
+            echo "Active nix develop"
+          '';
+        };
+      }
+    );
+}
